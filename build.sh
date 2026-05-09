d9221d6eaddd7a94e8ac6aa0303b3c6d94f29fe2
blackeagle686 <mathematecs1@gmail.com>
2026-05-09 22:48:53 +0300

    fix bug

--- build.sh
+++ build.sh
@@ -10,4 +10,12 @@ echo "Running migrations..."
 python3.11 manage.py makemigrations --noinput
 python3.11 manage.py migrate --noinput
 
+# Set Default Superuser Credentials
+export DJANGO_SUPERUSER_USERNAME="tlk@tlk.com"
+export DJANGO_SUPERUSER_EMAIL="tlk@tlk.com"
+export DJANGO_SUPERUSER_PASSWORD="tlk/1234/tlk.com"
+
+echo "Creating superuser..."
+python3.11 manage.py createsuperuser --noinput || true
+
 echo "Build complete."
